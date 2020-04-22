c8270fb1f8689776a2d407ad45fb7473013aa8bf
me <me@example.com>
2020-04-22 20:33:25 +0800

    fix error

--- print_state.py
+++ print_state.py
@@ -4,15 +4,15 @@ import json
 import base64
 import pickle
 
-from framework_lib.base_class import Pipe, Controller
-from framework_modules.oneforall import OneForAllWrap
-from framework_modules.check_cdn import CheckCDN
-from framework_modules.check_host_ssl import CheckHostSSL
-from framework_modules.dirsearch import Dirsearch
-from framework_modules.nmap_module import Nmap
-from framework_modules.generate_url import MasmapUrlGenerator
-from framework_modules.eyewitness import EyeWitness
-from framework_modules.whatweb import WhatWeb
+from framework_modules.base_class import Pipe, Controller
+from framework_modules.module_oneforall import OneForAllWrap
+from framework_modules.module_check_cdn import CheckCDN
+from framework_modules.module_check_host_ssl import CheckHostSSL
+from framework_modules.module_dirsearch import Dirsearch
+from framework_modules.module_nmap import Nmap
+from framework_modules.module_generate_url import MasmapUrlGenerator
+from framework_modules.module_eyewitness import EyeWitness
+from framework_modules.module_whatweb import WhatWeb
 
 with open('controller_state.json','r') as f:
     state = json.load(f)
