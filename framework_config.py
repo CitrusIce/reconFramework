db6ecc1b9121e871298af96d07ccaf39844f8e69
me <me@example.com>
2020-05-05 21:39:03 +0800

    move eyewitness_username into global config file

--- framework_config.py
+++ framework_config.py
@@ -2,4 +2,9 @@ mysql_host = "localhost"
 mysql_port = 3306
 mysql_user = "root"
 mysql_passwd = ""
+
 mysql_db = "reconFramework_db"
+
+# eyewitness need to run as non-root user
+# so you should specify a user existed for eyewitness to use
+eyewitness_username = ""
